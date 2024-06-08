3cf50cf015e8d56eceff610141762ea4f676a1e5
mubineurosia <mubineurosia@gmail.com>
2024-06-08 12:42:50 +0600

    remove image form about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import Image from 'next/image';
 
-const index = () => {
+const Index = () => {
   return (
     <div className='bg-[#252525] text-white font-Poppins container mx-auto px-6 py-16'>
       <section className='mb-16 text-center'>
@@ -8,7 +9,7 @@ const index = () => {
           About
         </h1>
         <p className='text-lg lg:text-xl text-gray-400'>
-          Eurosia Group is the largest B2B Brand tech company providing brand partners with a single dashboard to search, compare  Real Estate, Technology, Restaurant, Hotels, Travels Agency, food, home appliances Newspaper and Agro bees.
+          Eurosia Group is the largest B2B Brand tech company providing brand partners with a single dashboard to search, compare &amp; Real Estate, Technology, Restaurant, Hotels, Travels Agency, food, home appliances Newspaper and Agro bees.
         </p>
       </section>
 
@@ -17,7 +18,8 @@ const index = () => {
           History
         </h2>
         <p className='text-lg lg:text-xl text-gray-400 mb-4'>
-          Eurosia Group is the largest B2B Brand tech company providing brand partners with a single dashboard to search, compare, Real Estate, Technology, Restaurant, Hotels, Travels Agency, food, 
+          Eurosia Group is the largest B2B Brand tech company providing brand partners with a single dashboard to search, compare &amp; Real Estate, Technology, Restaurant, Hotels, Travels Agency, food, home appliances Newspaper and Agro bees. Eurosia Group Sister concern of Eurosia Holdings Limited is the largest B2B Brand Tech company providing brand partners with a single dashboard to search, compare &amp; Real Estate, Luxury Apartments, commercial flat sale. So no more thinking, I think that it is the turn to fulfill the dream. Eurosia Technologies Limited Sister concern of Eurosia Group. Eurosia Technologies Limited is the largest B2B Brand Tech company providing brand partners with a single dashboard to search, compare &amp; Technologies, software, Advertising Agency, Brand Promotion Creator, Digital Multimedia Agency And IT Training Center. We make sure and we call to again we make a Digital Bangladesh. Eurosia Group sister concern of Eurosia Restaurant Limited. Eurosia Restaurant is born is 19 November. Eurosia Restaurant Limited is the largest B2B Brand Tech company providing brand partners with a single dashboard to search, compare &amp; Restaurant, Luxury Hotels, food, and party center. Welcome to our Eurosia Restaurant. Our Best Achievement, Healthy food, Stylish Interior, Good Environment, Large Menu, Good Wine list, Big Dining and Creative Cuisine. Indian cuisine, Chinese and Creative unique food.
+        </p>
       </section>
 
       <section className='mb-12'>
@@ -25,12 +27,12 @@ const index = () => {
           Who we are!
         </h2>
         <p className='text-lg lg:text-xl text-gray-400'>
-          A powerful portfolio of the country’s most distinctive and selective developments, with an excellent reputation in the Real Estate market, and enviable relationships that give our clients exclusive access to the ultimate in luxury apartments and exquisite commercial spaces, all in prime locations of Dhaka city. Our promise remains to develop a portfolio of luxury spaces and offer investors as well as buyers an unparalleled quality of service, inimitable by competitors. It is the commitment to both impeccably high standards and attention to detail that drive us to our success. Besides building beyond expectation, Eurosia Group moreover sets standards for itself, for you and for others, which is why we proudly declare our driving philosophy to be "Setting Standards".
+          A powerful portfolio of the country’s most distinctive and selective developments, with an excellent reputation in the Real Estate market, and enviable relationships that give our clients exclusive access to the ultimate in luxury apartments and exquisite commercial spaces, all in prime locations of Dhaka city. Our promise remains to develop a portfolio of luxury spaces and offer investors as well as buyers an unparalleled quality of service, inimitable by competitors. It is the commitment to both impeccably high standards and attention to detail that drive us to our success. Besides building beyond expectation, Eurosia Group moreover sets standards for itself, for you and for others, which is why we proudly declare our driving philosophy to be &quot;Setting Standards&quot;.
         </p>
       </section>
 
       <section className='mb-12 text-center'>
-    
+        <Image src='/images/tm-logo.png' alt='Our Logo' width={500} height={500} className='mx-auto mb-8' />
         <h2 className='text-3xl lg:text-4xl font-semibold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-[#eaaaff] via-[#b5acff] to-[#b5acff]'>
           Our Logo
         </h2>
@@ -64,4 +66,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default Index;
